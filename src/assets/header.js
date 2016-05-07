fd8fa83bee111061a8e7485c13361f55814bd37d
The Zonebuilder <zone.builder@gmx.com>
2016-05-07 14:39:30 +0300

    Release 1.2.0

--- src/assets/header.js
+++ src/assets/header.js
@@ -1,5 +1,5 @@
 /*
-	JUL Comment System (JCS) version 1.1.9
+	JUL Comment System (JCS) version 1.2
 	Copyright (c) 2015 - 2016 The Zonebuilder (zone.builder@gmx.com)
 	http://sourceforge.net/projects/jul-comments/
 	Licenses: GPLv2 or later; LGPLv3 or later (http://sourceforge.net/p/jul-comments/wiki/License/)
